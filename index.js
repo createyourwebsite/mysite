e50d81b1e6d770375c77c258f375553dcdc054a9
createyourwebsite <111247184+createyourwebsite@users.noreply.github.com>
2022-09-08 17:18:56 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function classToggle() {
+	var el = document.querySelector('.icon-cards__content');
+	el.classList.toggle('step-animation');
+}
+
+document
+	.querySelector('#toggle-animation')
+	.addEventListener('click', classToggle);
